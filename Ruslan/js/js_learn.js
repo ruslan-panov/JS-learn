556224bbcb0a75c19422fd6b52b3b55729872f09
Panov Ruslan <ruslan.panov85@gmail.com>
2022-03-21 15:43:26 +0200

    javascript

---
+++ Ruslan/js/js_learn.js
@@ -0,0 +1,48 @@
+"use strict";   //строгий режим
+
+let number = 5;
+const leftBorderWidth = 1;
+number = 10;              
+console.log(number);
+
+
+     // Общение с пользователем
+
+// alert('Hello');
+
+// const result = confirm(' Are you here?');      подтверждение действия пользователем 
+// console.log(result);
+
+// const answer = prompt('How old are you?', '');    ответ пользователя
+// console.log(answer);
+
+// const answers = [];      ответы на вопросы пользователем(массив)
+
+// //  answers[0] = prompt('Your name' , ' ');
+// //  answers[1] = prompt('Your lastName ', ' ');
+// //  answers[2] = prompt('How old are you?', ' ');
+
+// //  document.write(answers);
+// console.log(typeof(answers) );
+
+            // Интерполяция
+
+//   const category = 'toys';
+  
+//   console.log(`http://someurl.com/${category}/5`);
+
+//   const user = 'Ruslan';
+
+//   alert(`Привет, ${user}`);
+            // Логические операторы
+
+
+// console.log(2*4 == 8);  // данное выражение ложь, 8 не равно 6
+// console.log(2*4 === '8');  //данное выражение правда
+
+
+// console.log(8%3);
+// console.log(9*9);
+
+console.log(2 + 2 * 2 !== 8 );
+console.log(2 + 2 * 2 === '6' );
