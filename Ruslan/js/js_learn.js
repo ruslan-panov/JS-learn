4614d8ac22ed234fe734af6d556896c3c9759ab2
Panov Ruslan <ruslan.panov85@gmail.com>
2022-03-23 21:10:08 +0200

    more teory

--- Ruslan/js/js_learn.js
+++ Ruslan/js/js_learn.js
@@ -2,15 +2,14 @@
 
 // let number = 5;
 // const leftBorderWidth = 1;
-// number = 10;              
+// number = 10;
 // console.log(number);
 
-
-     // Общение с пользователем
+// Общение с пользователем
 
 // alert('Hello');
 
-// const result = confirm(' Are you here?');      подтверждение действия пользователем 
+// const result = confirm(' Are you here?');      подтверждение действия пользователем
 // console.log(result);
 
 // const answer = prompt('How old are you?', '');    ответ пользователя
@@ -25,21 +24,19 @@
 // //  document.write(answers);
 // console.log(typeof(answers) );
 
-            // Интерполяция
+// Интерполяция
 
 //   const category = 'toys';
-  
+
 //   console.log(`http://someurl.com/${category}/5`);
 
 //   const user = 'Ruslan';
 
 //   alert(`Привет, ${user}`);
-            // Логические операторы
-
+// Логические операторы
 
 // console.log(2*4 == 8);  // данное выражение ложь, 8 не равно 6
 // console.log(2*4 === '8');  //данное выражение правда
-
 
 // console.log(8%3);
 // console.log(9*9);
@@ -51,7 +48,7 @@
 //  if (4 == 4) {
 //       alert('Все верно');
 //       console.log('Ok');
-      
+
 //  } else {
 //       alert('Ошибка');
 //       console.log('Error');
@@ -66,7 +63,7 @@
 //  }
 //  console.log(childsAllowance);
 
-// __ __ __ __ __ __ __ __ __ __ __ 
+// __ __ __ __ __ __ __ __ __ __ __
 
 //  var select = document.querySelector('select');
 // var para = document.querySelector('p');
@@ -88,59 +85,63 @@
 //     para.textContent = '';
 //   }
 // }
-// __ __ __ __ __ __ __ __ __ __ __ 
+// __ __ __ __ __ __ __ __ __ __ __
 
-  // const number = 50;                  switch делает точное сравнение
-  // switch (number) {
-  //   case 49:
-  //     console.log('Не верно');
-  //     break;
+// const number = 50;                  switch делает точное сравнение
+// switch (number) {
+//   case 49:
+//     console.log('Не верно');
+//     break;
 
-  //   case 55:
-  //     console.log('В другой раз');
-  //     break;
-    
-  //   case 50:
-  //     console.log('HIT');
-  //     break;
-  
-  //   default:
-  //     console.log('Не в этот раз');
-  //     break;
-  // }
+//   case 55:
+//     console.log('В другой раз');
+//     break;
 
-  // __ __ __ __ __ __ __ __ __ __ __     Цыклы    __ __ __ __ __ __ __ __ __ __ 
-  // Примеры
+//   case 50:
+//     console.log('HIT');
+//     break;
 
+//   default:
+//     console.log('Не в этот раз');
+//     break;
+// }
 
-  // let number = 50;
+// __ __ __ __ __ __ __ __ __ __ __     Цыклы    __ __ __ __ __ __ __ __ __ __
+// Примеры
 
-  // while (number <= 55) {
-  //   console.log(number);
-  //   number++;                    // Постоянно увеличивает чило на 1
-    
-  // }
+// let number = 50;
 
-  // let num = 50;
-  //  do {
-  //    console.log(num);
-  //    num++;
-  //  }
-  //  while (num < 55);
+// while (number <= 55) {
+//   console.log(number);
+//   number++;                    // Постоянно увеличивает чило на 1
 
+// }
 
-   let numb = 70;
+// let num = 50;
+//  do {
+//    console.log(num);
+//    num++;
+//  }
+//  while (num < 55);
 
-  //  for (let i = 2; i < 12; i++){
-  //  console.log(numb++);
-  // }
- 
-  for (let i = 1; i < 10; i++){
-  if ( i === 6) {
-    // break;
-    continue;          // продолжает выполнение операции пропустив число 'i'
-  }
-  console.log(i);
-}
-  
-  
+//    let numb = 70;
+
+//   //  for (let i = 2; i < 12; i++){
+//   //  console.log(numb++);
+//   // }
+
+//   for (let i = 1; i < 10; i++){
+//   if ( i === 6) {
+//     // break;
+//     continue;          // продолжает выполнение операции пропустив число 'i'
+//   }
+//   console.log(i);
+// }
+
+// const obj = {
+//   a1: 1,
+// };
+// for (let i = 1; i <= 100; i++) {
+//   obj["a" + i] = i;                        // obj['a1'] = 1
+// }
+// console.log(obj);
--- Ruslan/js/script.js
+++ Ruslan/js/script.js
@@ -21,7 +21,6 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
-
 /* Задание на урок:
 
 1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
@@ -36,51 +35,48 @@
 
 4) Потренироваться и переписать цикл еще двумя способами*/
 
-'use strict';
+"use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '' );
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 const personalMovieDB = {
-    count: numberOfFilms,
-    movies:{},
-    actors:{},
-    genres:[],
-    privat: false
-
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
 };
 
-// const a = prompt('Один из последних просмотренных фильмов?', ''),
-//       b = prompt('На сколько оцените его?', ''),
-//       c = prompt('Один из последних просмотренных фильмов?', ''),
-//       d = prompt('На сколько оцените его?', '');
+// const a = prompt("Один из последних просмотренных фильмов?", ""),
+//   b = prompt("На сколько оцените его?", "");
+// // c = prompt("Один из последних просмотренных фильмов?", ""),
+// // d = prompt("На сколько оцените его?", "");
 
-//       personalMovieDB.movies[a] = b;
-//       personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// // //personalMovieDB.movies[c] = d;
 
-//       console.log(personalMovieDB);
+// console.log(personalMovieDB);
 
-      for (let i = 0; i < 2; i++ ){
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-              b = prompt('На сколько оцените его?', '');
-              personalMovieDB.movies[a] = b;
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = prompt("На сколько оцените его?", "");
+  personalMovieDB.movies[a] = b;
 
-              if ( a != null && b != null && a != '' && b != '' && a.length < 50 ) {
-                personalMovieDB.movies[a] = b;
-                console.log('done');
-              } else {
-                console.log('Error');
-                i--;
-              }
-        }
-              if (personalMovieDB.count < 10) {
-                  console.log('Просмотрено довольно мало фильмов');
-                  
-              } else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30 ){
-                  console.log('Вы классический зритель');
-              } else if(personalMovieDB.count >=30){
-                  console.log('Вы киноман');
-              } else{
-                  console.log('Произошла ошибка');
-              }
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("done");
+  } else {
+    console.log("Error");
+    i--;
+  }
+}
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
+}
 
-             console.log(personalMovieDB);
-
+console.log(personalMovieDB);
