8209f62677da8a2adfa1f36fce736a02af3e48d3
Panov Ruslan <ruslan.panov85@gmail.com>
2022-03-28 18:45:45 +0300

    Lesson++

--- Ruslan/js/js_learn.js
+++ Ruslan/js/js_learn.js
@@ -1,4 +1,4 @@
-// "use strict";   //строгий режим
+// "use strict"; //строгий режим
 
 // let number = 5;
 // const leftBorderWidth = 1;
@@ -141,7 +141,165 @@
 // const obj = {
 //   a1: 1,
 // };
-// for (let i = 1; i <= 100; i++) {
+// for (let i = 5; i <= 100; i++) {
 //   obj["a" + i] = i;                        // obj['a1'] = 1
 // }
 // console.log(obj);
+// __ __ __ __ __ __ __ __ __ __ __     Цыклы  ЗАДАЧИ  __ __ __ __ __ __ __ __ __ __
+
+//  ___  ___  ___  ___  ___  ___ 1) Вывести числа от 4 до 400 на экран
+
+// let out = document.getElementById("out");
+// let tmp = "";
+// for (let i = 4; i < 400; i++) {
+//   tmp = tmp + i + "";
+// }
+// out.innerHTML = tmp;
+
+// __ __ __ __ __ __ __ __ __ __ __     Функции. Стрелочные функции(ES6)    __ __ __ __ __ __ __ __ __ __
+
+// function showFirstMessage() {
+//   console.log("Hello World");
+// }
+// showFirstMessage();
+
+// АНАЛОГИЧНАЯ ЗАПИСЬ ВЕРХНЕГО ВАРИАНТА
+
+// function showFirstMessage(numb) {
+//   console.log(numb);
+// }
+// showFirstMessage(123);
+
+// функции с переменными
+
+// let num = 20;
+// function showFirstMessage(text) {
+//   console.log(text);
+//   let num = 10;
+// }
+// showFirstMessage("Hello World");
+// console.log(num);
+
+// function calc(a, b, c) {
+//   return a * b + c; // !! После return - ничего не задается !!
+// }
+// console.log(calc(4, 5, 5));
+// console.log(calc(9, 7, 7));
+
+// function ret() {
+//   let num = 50;
+//   return num;
+// }
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// const q1 = function () {
+//   console.log("Hello");
+// };
+// q1();
+
+// const calc = (a, b, c) => a * b + c; // работает только для однострочных функций. В др.случае ставим {};
+// console.log(calc(2, 2, 3));
+
+// __ __ __ __ __ __ __ __ __ __ __    Методы и свойства сторок и чисел    __ __ __ __ __ __ __ __ __ __
+
+// const str = "Education start";
+// console.log(str.length); // выводит число символов в стоке с учетом пробелов
+
+// const arr = [1, 2, 3, 4, 5];
+// console.log(arr.length); // выводит число елементов в масиве
+
+// const st = "Education";
+// // console.log(st.toLowerCase());
+// console.log(st.toUpperCase());
+// console.log(st);
+
+// const fruit = "Some fruit";
+// console.log(fruit.indexOf("fruit")); // (ПОИСК ПОДСТРОКИ) ищет положение искомого в строке начиная с 0
+
+// const logg = "Hello World";
+// // console.log(logg.slice(6)); // вырезает елементы начиная с 6го(возможно вырезать по любой елемент (6, 11) поддерживает отрицательные значения)
+// // console.log(logg.substring(0, 5)); //тоже самое НО не поддерживает отрицательные значения
+// // console.log(logg.substr(6, 5)); // во втором аргументе данной команды мы показываем сколько символов начиная с 6го вырезать
+
+// // ОКРУГЛЕНИЕ!!
+
+// const num = 12.6;
+// console.log(Math.round(num)); // Математическая команда для округления чисел
+
+// const test = "12.2px";
+// console.log(parseInt(test)); // переводит значение в другую систему изчисления(В данном примере из пикселей в число)
+
+// __ __ __ __ __ __ __ __ __ __ __   Функции Callback    __ __ __ __ __ __ __ __ __ __
+
+// function first() {
+//   setTimeout(function () {
+//     console.log(1);
+//   }, 500); // задержка в выполнении функции в 500 милисекунд
+// }
+
+// function second() {
+//   console.log(2);
+// }
+// first();
+// second();
+
+// function LearnJS(lang, callback) {   //пример работы callback функции
+//   console.log(`Я учу: ${lang}`);
+//   callback();
+// }
+// LearnJS("JavaScript", function () {    // передача анонимной функции function()
+//   console.log("Я прошел этот урок");
+// });
+
+// function LearnJS(lang, callback) {
+//   console.log(`Я учу: ${lang}`);
+//   callback();
+// }
+
+// function done() {
+//   console.log("Я прошел этот урок");
+// }
+// LearnJS("JavaScript", done); // функция done является тем самым callback
+
+// __ __ __ __ __ __ __ __ __ __ __   Объекты, деструктуризация объектов (ES6)    __ __ __ __ __ __ __ __ __ __
+
+const options = {
+  name: "test",
+  width: 1024,
+  hight: 1024,
+  styles: {
+    bg: "red",
+    color: "black",
+  },
+  makeTest: function () {
+    console.log("Test");
+  },
+};
+options.makeTest();
+
+const { color, bg } = options.styles;
+console.log(bg);
+
+console.log(Object.keys(options.styles)); // выводит колличество елементов в массиве
+
+// delete options.styles; // удаляет выбранное свойство объекта
+// console.log(options);
+
+// for (let key in options) {               // Выводит записанные свойства в объект
+//   console.log(`Свойство ${key} имеет значение ${options[key]} `);
+// }
+// let counter = 0;
+// for (let key in options) {
+//   if (typeof options[key] === "object") {
+//     for (let i in options[key]) {
+//       //Вводим переменную для определения колличества елементов во вложенном массиве
+//       console.log(`Свойство ${i} имеет значение ${options[key][i]} `);
+//       counter++;
+//     }
+//   } else {
+//     console.log(`Свойство ${key} имеет значение ${options[key]} `);
+//     counter++;    // счетчик
+//   }
+// }
+// console.log(counter);
--- Ruslan/js/script.js
+++ Ruslan/js/script.js
@@ -35,48 +35,100 @@
 
 4) Потренироваться и переписать цикл еще двумя способами*/
 
-"use strict";
+/* Задание на урок:
 
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-};
+1) Первую часть задания повторить по уроку
 
-// const a = prompt("Один из последних просмотренных фильмов?", ""),
-//   b = prompt("На сколько оцените его?", "");
-// // c = prompt("Один из последних просмотренных фильмов?", ""),
-// // d = prompt("На сколько оцените его?", "");
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
 
-// personalMovieDB.movies[a] = b;
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
+// "use strict";
+
+// let numberOfFilms;
+
+// function start() {
+//   numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+//   while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+//     numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+//   }
+// }
+// start();
+
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false,
+// };
+
+// // const a = prompt("Один из последних просмотренных фильмов?", ""),
+// //   b = prompt("На сколько оцените его?", "");
+// // // c = prompt("Один из последних просмотренных фильмов?", ""),
+// // // d = prompt("На сколько оцените его?", "");
+
+// // personalMovieDB.movies[a] = b;
 // // //personalMovieDB.movies[c] = d;
 
-// console.log(personalMovieDB);
+// // console.log(personalMovieDB);
 
-for (let i = 0; i < 2; i++) {
-  const a = prompt("Один из последних просмотренных фильмов?", ""),
-    b = prompt("На сколько оцените его?", "");
-  personalMovieDB.movies[a] = b;
+// function rememberMyFilms() {
+//   for (let i = 0; i < 2; i++) {
+//     const a = prompt("Один из последних просмотренных фильмов?", ""),
+//       b = prompt("На сколько оцените его?", "");
+//     personalMovieDB.movies[a] = b;
 
-  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-    personalMovieDB.movies[a] = b;
-    console.log("done");
-  } else {
-    console.log("Error");
-    i--;
-  }
-}
-if (personalMovieDB.count < 10) {
-  console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-  console.log("Вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
-  console.log("Вы киноман");
-} else {
-  console.log("Произошла ошибка");
-}
+//     if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//       personalMovieDB.movies[a] = b;
+//       console.log("done");
+//     } else {
+//       console.log("Error");
+//       i--;
+//     }
+//   }
+// }
+// rememberMyFilms();
 
-console.log(personalMovieDB);
+// function detectPersonalLevel() {
+//   if (personalMovieDB.count < 10) {
+//     console.log("Просмотрено довольно мало фильмов");
+//   } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     console.log("Вы классический зритель");
+//   } else if (personalMovieDB.count >= 30) {
+//     console.log("Вы киноман");
+//   } else {
+//     console.log("Произошла ошибка");
+//   }
+// }
+// detectPersonalLevel();
+
+// // console.log(personalMovieDB);
+
+// // function showMyDB() {                     Моя запись!!!!!!!!!!!!!!
+// //   if (personalMovieDB.privat == false) {
+// //     console.log(personalMovieDB);
+// //   }
+// // }
+
+// function showMyDB(hidden) {
+//   if (!hidden) {
+//     console.log(personalMovieDB);
+//   } else {
+//     console.log("privat == true");
+//   }
+// }
+// showMyDB(personalMovieDB.privat);
+
+// function writeYourGenres() {
+//   for (let i = 1; i <= 3; i++) {
+//     const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+//     personalMovieDB.genres[i - 1] = genre; // запись [i - 1] по причине начала цикла для переменной i с единицы а не с ноля!!!!
+//   }
+// }
+// writeYourGenres();
